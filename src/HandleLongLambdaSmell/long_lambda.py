96ef325697d685dfcb217b4385d5d4282297bae8
jaywoo123 <jkim475@illinois.edu>
2019-04-13 18:23:35 -0500

    lambda merge resolved

--- src/HandleLongLambdaSmell/long_lambda.py
+++ src/HandleLongLambdaSmell/long_lambda.py
@@ -1,11 +1,7 @@
+
 import inspect
 import test
-
-# with open("test.py", "r") as f:
-#     content = f.readlines()
-#     for line in content:
-#         if "lambda" in line:
-#             print(line)
+import ast
 
 def get_long_lambda_source(lambda_func):
     try:
@@ -13,6 +9,7 @@ def get_long_lambda_source(lambda_func):
         print (source_lines)
     except IOError:
         return None
+
     if len(source_lines) > 10:
         return source_lines
     return None
@@ -28,3 +25,18 @@ def get_short_lambda_ast_node(lambda_func):
         source_ast = ast.parse(source_text)
         return next((node for node in ast.walk(source_ast)
                      if isinstance(node, ast.Lambda)), None)
+
+    if len(source_lines) > 1:
+        return None
+    return source_lines
+
+
+from pprint import pprint
+
+with open('test.py') as f:
+    data = f.read()
+    module = ast.parse(data)
+    function = module.body[0]
+    for obj in function.body:
+        print(obj)
+
