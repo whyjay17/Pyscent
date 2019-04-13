594001464dee2ac8df1d5d5e7e993b3f28193f8d
jaywoo123 <jkim475@illinois.edu>
2019-04-13 17:40:51 -0500

    lambda example

--- src/HandleLongLambdaSmell/test.py
+++ src/HandleLongLambdaSmell/test.py
@@ -1,6 +1,7 @@
 def test_lambda():
     # rebind the paint function to implement curriculum learning
-        test1 = lambda text: (text %2 == 0)
+        test1 = lambda text: (0 if text % 2 == 0 else
+                              1)
         test2 = lambda text: \
             text % 2 == 0
         test3 = lambda text: text %2 == 0
