5ce6ad5d9a1a293fc267b19a0a50fa118d8f332e
jaywoo123 <jkim475@illinois.edu>
2019-04-17 03:37:49 -0500

    working on cohesino detector

---
+++ src/Detector/class_coupling_detector.py
@@ -0,0 +1,13 @@
+import class_cohesion as ch
+import os
+
+
+def detect_class_cohesion(directory, limit):
+    path, dirs, files = next(os.walk(directory))
+    output = ch.output_class_cohesion(directory, limit)
+    print(str(output.stdout).split())
+    print(float(str(output.stdout).split().count("Total:")))
+    print("number of files with cohesion below " + str(limit) + "% : " + str(float(str(output.stdout).split().count("Total:")) / len(files)))
+    # print (output.stdout)
+
+detect_class_cohesion("/Users/jaewookim/PycharmProjects/cs527_project/code-dump/flask-master", 50)
