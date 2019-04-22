b906501fcc1fafcbbec89cac367cebfb0ba8115f
jaywoo123 <jkim475@illinois.edu>
2019-04-22 15:43:42 -0500

    merging

--- src/Detector/CodeSmellHandlers/HandleShotgunSurgerySmell/shotgun_surgery.py
+++ src/Detector/CodeSmellHandlers/HandleShotgunSurgerySmell/shotgun_surgery.py
@@ -1,4 +1,4 @@
-# Shotgun Surgery: 
+# Shotgun Surgery:
 ## A shotgun surgery can be when a change to a "Class A" causes several minor changes to other classes
 ## Shotgun surgery happens when you have to make many changes in your codebase to achieve seemingly simple tasks
 
@@ -10,16 +10,17 @@ import ast
 import astor
 import collections
 
+
 def detect_shotgun_surgery_per_file(file_path):
-    """ 
+    """
     Detect Shotgun Surgery: number of external function calls within a class
-    
-    Parameters: 
+
+    Parameters:
         file_path (string): path of the source code file to be detected
-    
+
     Return:
         analysis (dict[string]: (string, bool)): ClassName: (number of external calls / total calls, isSmelly)
-    
+
     """
     # TODO: exclude function calls from stdlib or pip-packages
     analysis = collections.defaultdict(list)
@@ -32,22 +33,20 @@ def detect_shotgun_surgery_per_file(file_path):
                 for classObj in instance.body:
                     if isinstance(classObj, ast.FunctionDef):
                         functions.append(classObj.name)
-                        
+
                 for classObj in ast.walk(instance):
                     if isinstance(classObj, ast.Call):
                         if astor.to_source(classObj).split('(')[0].split('.')[-1] not in functions and \
-                            astor.to_source(classObj).split('(')[0].split('.')[-1] not in dir(__builtins__):
+                                astor.to_source(classObj).split('(')[0].split('.')[-1] not in dir(__builtins__):
                             external_count += 1
                             analysis[instance.name].append(classObj.lineno)
                         total_count += 1
-                
-                if external_count > 5: # n = 5
+
+                if external_count > 5:  # n = 5
                     analysis[instance.name].append('{}/{} (external calls / total)'.format(external_count, total_count))
                     # analysis[instance.name].append(True if external_count > 0 else False)
                 # TODO: decide metric n
-    
+
     return analysis
-
-
 
 # ana = detect_shotgun_surgery_per_file('sample_file.py')
--- src/Detector/shotgun_surgery_detector.py
+++ src/Detector/shotgun_surgery_detector.py
@@ -7,7 +7,7 @@ from CodeSmellHandlers.HandleShotgunSurgerySmell.shotgun_surgery import detect_s
 def detect_shotgun_surgery(directory):
     output_list = output_shotgun_surgery(directory)
     num_smelly_class, top = shotgun_output_formatter(output_list)
-    
+
     return num_smelly_class, top
 
 
@@ -16,15 +16,15 @@ def shotgun_output_formatter(output_list):
     most_external_call = 0
     for key in output_list:
         smelly_class += len(output_list[key])
-        
+
     for fileName in output_list:
         for className in output_list[fileName]:
             if most_external_call < len(output_list[fileName][className]) - 1:
                 most_external_call = len(output_list[fileName][className])
                 top_class = (fileName, className, most_external_call)
-    
+
     return smelly_class, top_class
-    
+
 
 def output_shotgun_surgery(directory):
     output_list = collections.defaultdict(list)
@@ -33,7 +33,7 @@ def output_shotgun_surgery(directory):
             ss = detect_shotgun_surgery_per_file(directory + "/" + filename)
             if len(ss) > 0:
                 output_list[filename] = ss
-    
+
     return output_list
 
 # test run: remove later
